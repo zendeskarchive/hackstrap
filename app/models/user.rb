03ba1042c5fb02f06337a665373c7782f905dab6
Marcin Bunsch <marcin@futuresimple.com>
2012-03-10 17:55:40 +0100

    Cleaning up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  extend Github
-
   attr_accessible :username, :avatar_url
 
 end
--- spec/models/auth/github_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe User::Github do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
